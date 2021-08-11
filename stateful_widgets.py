02da65887c54aa1ef05affcdb7d3b0dff3e3d76e
Amanda Walker <amanda@amandawalker.io>
2021-08-11 12:48:16 -0700

    Demo decorator approach
    Meh, not worth it. Making it actually work for each widget would require
    passing in a value reconciliation function tailored for each, and with
    the amount of checking for arguments existing required to handle the
    functions receiving a very generic set of arguments in practice, it
    doesn't seem to add any value over writing it all out.

--- stateful_widgets.py
+++ stateful_widgets.py
@@ -10,3 +10,15 @@ def wrapped_multiselect(label, options, default_values, key):
 
     multiselect = st.multiselect(label, options, values, key=key)
     return multiselect
+
+def stateful(widget_fn):
+    def stateful_widget(*args, options, value, key, **kwargs):
+        values = state.get(key, value)
+
+        values = [v for v in values if v in options]
+        return widget_fn(*args, options, values, key=key, **kwargs)
+    return stateful_widget
+
+@stateful
+def wrapped_multiselect_2(*args, **kwargs):
+    return st.multiselect(*args, **kwargs)
